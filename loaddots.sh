74ee4921b7f12191675e176aa47de402328a50bf
jhxnnat <jhonnaromaniteran@gmail.com>
2025-02-05 21:25:25 -0500

    load codium (vscode) config

--- loaddots.sh
+++ loaddots.sh
@@ -2,3 +2,5 @@
 
 cp /home/jhxnnat/.config/lvim/config.lua ./lvim/config.lua
 
+cp /home/jhxnnat/.config/VSCodium/User/settings.json ./vscode/settings.json
+cp /home/jhxnnat/.config/VSCodium/User/keybindings.json ./vscode/keybindings.json
