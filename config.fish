61496032cb6f1ed852bec969132e498f5bcc7fae
Jhxnnat <jhonnaromaniteran@gmail.com>
2025-08-22 18:34:32 -0500

    Vie 22 ago

--- config.fish
+++ config.fish
@@ -1,36 +1,15 @@
-# set -g fish_key_bindings fish_vi_key_bindings
 set fish_cursor_insert block
+set -U fish_prompt_pwd_dir_length 0
 
-alias neofetch="fastfetch -c ~/Dev/terceros/neofetch.jsonc"
+alias neofetch="fastfetch -c ~/.config/fastfetch/neofetch.jsonc"
 alias n="nvim"
 
 function fish_greeting
-	random choice "Debian btw" "When ReD?" "175 = 300"
+	random choice "Rolling Release btw" "When Music?" "175 = 300"
 end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-end
-
-function fish_mode_prompt
-  switch $fish_bind_mode
-    case default
-      set_color --bold red
-      echo ' '
-    case insert
-      set_color --bold green
-      echo 'i '
-    case replace_one
-      set_color --bold green
-      echo 'r '
-    case visual
-      set_color --bold brmagenta
-      echo 'v '
-    case '*'
-      set_color --bold red
-      echo '? '
-  end
-  set_color normal
 end
 
 function fish_prompt
@@ -38,12 +17,8 @@ function fish_prompt
     # Prompt status only if it's not 0
     set -l stat
     if test $last_status -ne 0
-        set stat (set_color red)"$last_status! "(set_color normal)
+        set stat (set_color red)" ($last_status)" (set_color normal)
     end
 
-    string join '' -- (set_color green) (prompt_pwd) (set_color normal) (fish_git_prompt)' ' $stat ' > '
-end
-
-function fish_right_prompt
-	echo (date '+%H:%M')
+    string join '' -- (set_color --bold green) (prompt_pwd) (set_color normal) (fish_git_prompt) $stat ' > '
 end
---
+++ nvim/nvim/lua/plugin/lualine.lua
@@ -0,0 +1,23 @@
+local lualine = require("lualine")
+
+local function neovimlogo()
+	return [[ ]]
+end
+
+lualine.setup {
+    options = {
+        theme = 'zenbones',
+        section_separators = { left = '', component_separators = '' },
+        refresh = {
+              statusline = 50,
+        }
+    },
+	sections = {
+	    lualine_a = { neovimlogo, 'mode'},
+	    lualine_b = {'branch'},
+	    lualine_c = {'filename'},
+	    lualine_x = {'encoding'},
+	    lualine_y = {'selectioncount', 'searchcount', 'progress'},
+	    lualine_z = {'location'}
+	}
+}
